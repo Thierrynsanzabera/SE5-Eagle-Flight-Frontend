6fdf7e77e5412f9461741c57437d1c5a9825eedd
ShadowCore <JohnathonLCortez@yahoo.com>
2025-02-20 11:30:05 -0600

    Basic Account Information Page Made

--- eagle-flight/src/views/AccountInformation.vue
+++ eagle-flight/src/views/AccountInformation.vue
@@ -1,22 +1,75 @@
 <template>
     <v-container>
-      <v-row justify="center" class="mt-10">
-        <v-col cols="12" class="text-center">
-          <h1>Account Information</h1>
-        </v-col>
-      </v-row>
+        <!-- Header row -->
+        <v-row justify="center" class="mt-10">
+            <v-col cols="12" class="text-center">
+                <h1>Account Information</h1>
+            </v-col>
+        </v-row>
+        <v-row justify="center">
+            <!-- Left column (Personal Information) -->
+            <v-col cols="12" md="4">
+                <v-card>
+                    <v-card-title class="headline">Personal Information</v-card-title>
+                    <v-card-text class="card-text-box">
+                        <br>
+                        <p>First Name:</p>
+                        <p>Last Name:</p>
+                        <p>Home Address:</p>
+                        <p>Phone Number:</p>
+                        <p>Other Information:</p>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+  
+            <!-- Right column (Awards, Badges, Points) -->
+            <v-col cols="12" md="4">
+                <!-- Awards -->
+                <v-card class="mb-4">
+                    <v-card-title class="headline">Awards</v-card-title>
+                    <v-card-text class="card-text-box">
+                        <br>
+                        <p>List of Awards</p>
+                    </v-card-text>
+                </v-card>
+                <!-- Badges -->
+                <v-card class="mb-4">
+                    <v-card-title class="headline">Badges</v-card-title>
+                    <v-card-text class="card-text-box">
+                        <br>
+                        <p>Badge Icons/Tasks</p>
+                    </v-card-text>
+                </v-card>
+                <!-- Points -->
+                <v-card>
+                    <v-card-title class="headline">Points</v-card-title>
+                    <v-card-text class="card-text-box">
+                        <br>
+                        <p>Total:</p>
+                        <p>Lifetime:</p>
+                        <p>Used:</p>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
     </v-container>
-  </template>
-  
-  <script>
-  export default {
-    name: 'Account Information Page'
-  }
-  </script>
-  
-  <style scoped>
-  .mt-10 {
-    margin-top: 10rem;
-  }
-  </style>
-  
+</template>
+
+<script>
+    export default {
+        name: 'AccountInformationPage',
+    }
+</script>
+
+<style scoped>
+    .headline {
+        background-color:#800000;
+        color: white;
+    }
+    .mt-10 {
+        margin-top: 10rem;
+    }
+    .card-text-box {
+        background-color: lightgrey;
+    }
+</style>  
