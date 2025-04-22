cf15fb92c9af5be676677a06c2ba0a4a00e9392d
ShadowCore <JohnathonLCortez@yahoo.com>
2025-04-22 12:03:04 -0500

    Event Students open and closing fix
    Event Students now opens and closes on the left side without shifting the events list.

--- eagle-flight/src/views/Events.vue
+++ eagle-flight/src/views/Events.vue
@@ -4,17 +4,31 @@
         <ExpandedCombo />
     </v-overlay>
     <v-card color="transparent" height="64"></v-card>
-    <v-container class="d-flex justify-center align-center">
-        <EventList @event-selected="passToEdit" class="mx-3" />
+    <v-container>
+        <v-row>
+            <v-col cols="3">
+                <div v-if="selectedEvent">
+                    <EventStudents :event-id="selectedEvent.id" />
+                </div>
+            </v-col>
+            <v-col cols="6">
+                <EventList @event-selected="passToEdit" class="mx-3" />
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { computed } from "vue";
 import { useEditEventStore } from "@/store/editEventStore";
+import { useEventStore } from "@/store/eventStore";
+import EventList from "@/components/fp_edit_components/EventList.vue";
+import EventStudents from "@/components/fp_edit_components/EventStudents.vue";
+import ExpandedCombo from "@/components/fp_edit_components/ExpandedCombo.vue";
 
 const editEventStore = useEditEventStore();
-const selectedEvent = ref(null);
+const eventStore = useEventStore();
+const selectedEvent = computed(() => eventStore.selectedEvent);
 
 function passToEdit(event) {
     selectedEvent.value = event;
